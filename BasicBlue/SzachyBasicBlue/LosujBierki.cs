5b3db7abab1284c1e3522642c6fa137644d8e5b4
adi22 <adam15as@wp.pl>
2014-01-17 21:12:03 +0100

    LosujBierki
    Generator losowości wyboru figury

---
+++ BasicBlue/SzachyBasicBlue/LosujBierki.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SzachyBasicBlue;
+
+namespace BasicBlue.SzachyBasicBlue
+{
+    class LosujBierki
+    {
+        // Adam Sieradzki, Michał Łuczyński
+
+
+        public static List<Bierka> zwrocListe(int ile, List<Bierka> lista)
+        {
+            while (lista.Count > ile)
+            {
+                Random rnd = new Random();
+                int r = rnd.Next(lista.Count);
+                lista.RemoveAt(r);
+            }
+            return lista;
+        }
+
+
+
+    }
+}
